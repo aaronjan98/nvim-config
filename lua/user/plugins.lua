b356a68c9dd23fe132f24c501c1438ee5a8fa06d
aaronjan98 <aaronjan98@gmail.com>
2022-09-30 10:14:59 -0700

    add lazy load plugins like vim-dispatch

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -23,7 +23,6 @@ vim.cmd [[
   augroup end
 ]]
 
-
 -- Use a protected call so we don't error out on first use
 local status_ok, packer = pcall(require, "packer")
 if not status_ok then
@@ -34,104 +33,130 @@ end
 packer.init {
   display = {
     open_fn = function()
-      return require("packer.util").float { border = "rounded" }
+      return require("packer.util").float({ border = "none" })
     end,
+    prompt_border = "rounded",
   },
 }
 
 -- Install your plugins here
-return packer.startup(function(use)
-  -- Plugin Mangager
-  use "wbthomason/packer.nvim" -- Have packer manage itself
+return packer.startup({
+  function(use)
+    -- Plugin Mangager
+    use "wbthomason/packer.nvim" -- Have packer manage itself
 
-  -- Lua Development
-  use "nvim-lua/plenary.nvim" -- Useful lua functions used in lots of plugins
-  use "nvim-lua/popup.nvim" -- An implementation of the Popup API from vim in Neovim
+    -- Lua Development
+    use "nvim-lua/plenary.nvim" -- Useful lua functions used in lots of plugins
+    use "nvim-lua/popup.nvim" -- An implementation of the Popup API from vim in Neovim
 
-  ----colorschemes
-  --use "lunarvim/onedarker.nvim"
-  --use { "catppuccin/nvim", as = "catppuccin" }
-  --use "folke/tokyonight.nvim"
-  --use "shaunsingh/moonlight.nvim"
+    -- Lazy loading:
+    -- Load on specific commands
+    use {'tpope/vim-dispatch', opt = true, cmd = {'Dispatch', 'Make', 'Focus', 'Start'}}
 
-  -- use "lewis6991/impatient.nvim" -- fast startup
+    -- Load on a combination of conditions: specific filetypes or commands
+    -- Also run code after load (see the "config" key)
+    use {
+      'w0rp/ale',
+      ft = {'sh', 'zsh', 'bash', 'c', 'cpp', 'cmake', 'html', 'markdown', 'racket', 'vim', 'tex'},
+      cmd = 'ALEEnable',
+      config = 'vim.cmd[[ALEEnable]]'
+    }
 
-  -- -- lsp config
-  -- use "neovim/nvim-lspconfig"
-  -- use "williamboman/nvim-lsp-installer"
+    -- lazy load plugins in .local/share/nvim/site/pack/packer/opt
+    use {'iamcco/markdown-preview.nvim', run = 'cd app && npm install', cmd = 'MarkdownPreview'}
 
-  -- use "hrsh7th/nvim-pasta" -- Cycle yank history
-  use "hrsh7th/nvim-cmp" -- Completion plugin
-  use "hrsh7th/cmp-nvim-lsp" -- cmp lsp plugin
-  use "hrsh7th/cmp-nvim-lua" -- cmp lua
-  -- use "hrsh7th/cmp-buffer" -- buffer completions
-  -- use "hrsh7th/cmp-path" -- path completions
-  use "hrsh7th/cmp-cmdline" -- cmdline opts
-  use "hrsh7th/cmp-nvim-lsp-signature-help" -- cmdline opts
-  use "saadparwaiz1/cmp_luasnip" -- snippet completions
-  -- use {
-	-- "windwp/nvim-autopairs",
-  --   config = function() require("nvim-autopairs").setup {} end
-  -- }
-  -- use "numToStr/Comment.nvim" -- Comment
-  -- use "JoosepAlviste/nvim-ts-context-commentstring" -- String ts comment
-  -- use "lewis6991/gitsigns.nvim" -- gitsigns
-  -- use "kyazdani42/nvim-web-devicons" -- tree webicons
-  -- use "kyazdani42/nvim-tree.lua" -- nvim tree
-  -- use {'akinsho/bufferline.nvim', tag = "v2.*", requires = 'kyazdani42/nvim-web-devicons'}
-  -- use "moll/vim-bbye"
-  -- use "jose-elias-alvarez/null-ls.nvim" -- for formatters and linters
-  -- use "ahmedkhalf/project.nvim"
-  -- use "nvim-lualine/lualine.nvim"
-  -- use "simrat39/symbols-outline.nvim" -- tags
-  -- use "lukas-reineke/indent-blankline.nvim" -- line indent
-  -- use {"akinsho/toggleterm.nvim", tag = 'v2.*', config = function()
-  --   require("toggleterm").setup() end}
-  -- use "nvim-treesitter/nvim-treesitter-refactor"
+    ----colorschemes
+    --use "lunarvim/onedarker.nvim"
+    --use { "catppuccin/nvim", as = "catppuccin" }
+    --use "folke/tokyonight.nvim"
+    --use "shaunsingh/moonlight.nvim"
 
-  -- use {
-  --   "weilbith/nvim-code-action-menu",
-  --   cmd = "CodeActionMenu",
-  --   config = function()
-  --     vim.g.code_action_menu_show_diff = false
-  --   end,
-  -- } -- better code action menu
+    -- use "lewis6991/impatient.nvim" -- fast startup
 
-  -- -- Cursorhold fix
-  -- use {
-  --   "antoinemadec/FixCursorHold.nvim",
-  --   event = "BufRead",
-  --   config = function()
-  --     vim.g.cursorhold_updatetime = 400
-  --   end,
-  -- }
+    -- -- lsp config
+    -- use "neovim/nvim-lspconfig"
+    -- use "williamboman/nvim-lsp-installer"
 
-  -- snippets
-  use "L3MON4D3/LuaSnip" -- snip engine
-  use "rafamadriz/friendly-snippets" -- a bunch of snippets to use
+    -- use "hrsh7th/nvim-pasta" -- Cycle yank history
+    use "hrsh7th/nvim-cmp" -- Completion plugin
+    use "hrsh7th/cmp-nvim-lsp" -- cmp lsp plugin
+    use "hrsh7th/cmp-nvim-lua" -- cmp lua
+    -- use "hrsh7th/cmp-buffer" -- buffer completions
+    -- use "hrsh7th/cmp-path" -- path completions
+    use "hrsh7th/cmp-cmdline" -- cmdline opts
+    use "hrsh7th/cmp-nvim-lsp-signature-help" -- cmdline opts
+    use "saadparwaiz1/cmp_luasnip" -- snippet completions
+    -- use {
+    -- "windwp/nvim-autopairs",
+    --   config = function() require("nvim-autopairs").setup {} end
+    -- }
+    -- use "numToStr/Comment.nvim" -- Comment
+    -- use "JoosepAlviste/nvim-ts-context-commentstring" -- String ts comment
+    -- use "lewis6991/gitsigns.nvim" -- gitsigns
+    -- use "kyazdani42/nvim-web-devicons" -- tree webicons
+    -- use "kyazdani42/nvim-tree.lua" -- nvim tree
+    -- use {'akinsho/bufferline.nvim', tag = "v2.*", requires = 'kyazdani42/nvim-web-devicons'}
+    -- use "moll/vim-bbye"
+    -- use "jose-elias-alvarez/null-ls.nvim" -- for formatters and linters
+    -- use "ahmedkhalf/project.nvim"
+    -- use "nvim-lualine/lualine.nvim"
+    -- use "simrat39/symbols-outline.nvim" -- tags
+    -- use "lukas-reineke/indent-blankline.nvim" -- line indent
+    -- use {"akinsho/toggleterm.nvim", tag = 'v2.*', config = function()
+    --   require("toggleterm").setup() end}
+    -- use "nvim-treesitter/nvim-treesitter-refactor"
 
-  -- -- run scripts asynchronously
-  -- use "skywind3000/asyncrun.vim"
-  -- -- notify popup plugin
-  -- use "rcarriga/nvim-notify"
-  -- -- Telescope
-  -- use "nvim-telescope/telescope.nvim"
-  -- use "nvim-telescope/telescope-media-files.nvim"
-  -- -- Treesitter
-  -- use {
-  --   "nvim-treesitter/nvim-treesitter",
-  --   run = ":TSUpdate",
-  -- }
-  -- -- Mark lines plugin
-  -- use "chentoast/marks.nvim"
-  -- -- Colorizer, hex color etc.
-  -- use "norcalli/nvim-colorizer.lua"
-  -- -- smooth scrolling
-  -- use "karb94/neoscroll.nvim"
+    -- use {
+    --   "weilbith/nvim-code-action-menu",
+    --   cmd = "CodeActionMenu",
+    --   config = function()
+    --     vim.g.code_action_menu_show_diff = false
+    --   end,
+    -- } -- better code action menu
 
-  -- Automatically set up your configuration after cloning packer.nvim
-  -- Put this at the end after all plugins
-  if PACKER_BOOTSTRAP then
-    require("packer").sync()
-  end
-end)
+    -- -- Cursorhold fix
+    -- use {
+    --   "antoinemadec/FixCursorHold.nvim",
+    --   event = "BufRead",
+    --   config = function()
+    --     vim.g.cursorhold_updatetime = 400
+    --   end,
+    -- }
+
+    -- snippets
+    use "L3MON4D3/LuaSnip" -- snip engine
+    use "rafamadriz/friendly-snippets" -- a bunch of snippets to use
+
+    -- -- run scripts asynchronously
+    -- use "skywind3000/asyncrun.vim"
+    -- -- notify popup plugin
+    -- use "rcarriga/nvim-notify"
+    -- -- Telescope
+    -- use "nvim-telescope/telescope.nvim"
+    -- use "nvim-telescope/telescope-media-files.nvim"
+    -- -- Treesitter
+    -- use {
+    --   "nvim-treesitter/nvim-treesitter",
+    --   run = ":TSUpdate",
+    -- }
+    -- -- Mark lines plugin
+    -- use "chentoast/marks.nvim"
+    -- -- Colorizer, hex color etc.
+    -- use "norcalli/nvim-colorizer.lua"
+    -- -- smooth scrolling
+    -- use "karb94/neoscroll.nvim"
+
+    -- Automatically set up your configuration after cloning packer.nvim
+    -- Put this at the end after all plugins
+    if PACKER_BOOTSTRAP then
+      require("packer").sync()
+    end
+  end,
+
+  config = {
+    display = {
+      open_fn = function()
+        return require('packer.util').float({ border = 'none' })
+      end
+    }},
+})
